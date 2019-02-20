c922ac19c66039d3512f1ee845a097bb5253bb06
Jonny McFadden <jmcfadden1528@gmail.com>
2019-02-20 17:49:51 -0600

    Completed-MonthAssignment.Lesson04

--- Month/Program.cs
+++ Month/Program.cs
@@ -10,9 +10,71 @@ namespace Month
 
             // Save the input to a variable.
 
+            string inputmonthNum = Console.ReadLine();
+
             // Use the if statment to update the monthName variable based on the user's input.
             string monthName = string.Empty;
+            
+            
+            if (inputmonthNum == "1")
+                {
+                monthName = "January";
+            }
 
+            else if (inputmonthNum == "2")
+            {
+                monthName = "February";
+            }
+
+            else if (inputmonthNum == "3")
+            {
+                monthName = "March";
+            }
+
+            else if (inputmonthNum == "4")
+            {
+                monthName = "April";
+            }
+
+            else if (inputmonthNum == "5")
+            {
+                monthName = "May";
+            }
+
+            else if (inputmonthNum == "6")
+            {
+                monthName = "June";
+            }
+
+            else if (inputmonthNum == "7")
+            {
+                monthName = "July";
+            }
+
+            else if (inputmonthNum == "8")
+            {
+                monthName = "August";
+            }
+
+            else if (inputmonthNum == "9")
+            {
+                monthName = "September";
+            }
+
+            else if (inputmonthNum == "10")
+            {
+                monthName = "October";
+            }
+
+            else if (inputmonthNum == "11")
+            {
+                monthName = "November";
+            }
+
+            else if (inputmonthNum == "12")
+            {
+                monthName = "December";
+            }
 
             Console.WriteLine($"The name of the month you entered is {monthName}");
 
